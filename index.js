3bf5d6d7d46638685ec3b459aa22f4f4f36735ec
jookyboi <rjiang.bb@gmail.com>
2017-08-22 15:05:52 -0400

    Better initial typing behavior.

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const alfy = require('alfy');
 const CACHER_API = 'http://api.cacher.dev';
 
 const CACHE_MAX_AGE = 1000 * 60;
+
 const API_KEY = process.env['CACHER_API_KEY'];
 const API_TOKEN = process.env['CACHER_API_TOKEN'];
+
+const FETCH_OPTIONS = {
+    headers: {
+        'X-Api-Key': API_KEY,
+        'X-Api-Token': API_TOKEN
+    },
+    maxAge: CACHE_MAX_AGE
+};
 
 if (!API_KEY || !API_TOKEN) {
     alfy.error('Set "CACHER_API_KEY" and "CACHER_API_TOKEN" workflow variables.');
@@ -12,13 +21,7 @@ if (!API_KEY || !API_TOKEN) {
 
 alfy.fetch(
     `${CACHER_API}/integrations/show_all?user_id=21`,
-    {
-        headers: {
-            'X-Api-Key': API_KEY,
-            'X-Api-Token': API_TOKEN
-        },
-        maxAge: CACHE_MAX_AGE
-    }
+    FETCH_OPTIONS
 ).then(data => {
     let teamSnippets = data.teams.reduce((allSnippets, team) => {
         return allSnippets.concat(team.library.snippets);
@@ -26,15 +29,13 @@ alfy.fetch(
 
     let allSnippets = data.personalLibrary.snippets.concat(teamSnippets);
 
-    // todo: fill in quicklook content
-
     const items = alfy
         .inputMatches(allSnippets, 'title')
         .map(x => {
-            let firstFileContent = x.files[0].content;
+            let firstFile = x.files[0];
+            let firstFileContent = firstFile.content;
 
             return {
-                uid: x.guid,
                 title: x.title,
                 subtitle: firstFileContent,
                 text: {
